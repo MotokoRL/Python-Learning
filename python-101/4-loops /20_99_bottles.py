177cc0c28380388498b9946715d50e07fc551db3
MotokoRL <72717786+MotokoRL@users.noreply.github.com>
2025-06-15 22:37:54 +0800

    Create 20_99_bottles.py
    my answer for the question.

---
+++ python-101/4-loops /20_99_bottles.py
@@ -0,0 +1,3 @@
+# Write code below 💖
+for i in range (99):
+  print(f' {99 - i} bottles of beer on the wall \n {99 - i} bottles of beer \n take one down, pass it around \n {99 - (i + 1)} bottles of beer on the wall')
